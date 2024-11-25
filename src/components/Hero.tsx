17572d26bb40173c292800783e845e8eb81c7ce9
YASH VIRAJ PANSARE <pansarey91@gmail.com>
2024-11-25 15:30:36 +0530

    updated package.json

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { motion } from 'framer-motion';
 import { Sparkles, Target, Compass } from 'lucide-react';
 import { Swiper, SwiperSlide } from 'swiper/react';
